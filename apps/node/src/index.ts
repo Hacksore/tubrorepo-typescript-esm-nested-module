a199104a65193651c1621d402a8905e1139adb6b
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-04-18 09:01:30 -0500

    Use import from same pkg

--- apps/node/src/index.ts
+++ apps/node/src/index.ts
@@ -1,7 +1,10 @@
 import { getCoreVersion } from "@acme/api/core";
 import { getGithubVersion } from "@acme/api/github";
+import { TIME_STAMP } from "./utils";
 
 setInterval(() => {
   console.log("core", getCoreVersion());
   console.log("github", getGithubVersion());
+  console.log(TIME_STAMP)
+
 }, 1000);
